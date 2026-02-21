b3bd972436a08b636a76437ecfc4c12400cb3e26
nadilHesara <nadilhesara@gmail.com>
2026-02-21 18:07:14 +0530

    Done some animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,46 +3,60 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
 
     [Header("Movement Info")]
     [SerializeField] private float jumpForce;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private bool runBegun;
+    
+    
+    private bool playerUnlocked;
 
     [Header("Collsion Info")]
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
-    [SerializeField] private bool isGrounded;
+    private bool isGrounded;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (runBegun)
+        AnimatorControllers();
+
+        if (playerUnlocked)
             rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
 
-
-        CheckInput();
         CheckCollision();
 
+        CheckInput();
+
+    }
+
+    private void AnimatorControllers()
+    {
+
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void CheckCollision()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);    
     }
 
     private void CheckInput()
     {
         if (Input.GetButtonDown("Fire2"))
-            runBegun = true;
+            playerUnlocked = true;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
